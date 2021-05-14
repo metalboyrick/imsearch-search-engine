e1fb2d09f61769dd05a85e317bdc0fc0f308b952
DESKTOP-S551RPR\Richard <rsulisthio@gmail.com>
2021-05-14 23:35:22 +0700

    Added models for search service

--- server/imsearch/search_service/models.py
+++ server/imsearch/search_service/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_id = models.CharField(max_length=100,unique=True, primary_key= True, blank=False)
+    image_path = models.CharField(max_length=155, unique=True, blank=False)
+
+class Label(models.Model):
+    label_id = models.CharField(max_length=100,unique=True, primary_key= True, blank=False)
+    label_name = models.CharField(max_length=155, unique=False, blank=False)
+
+class Relationship(models.Model):
+    rel_id = models.CharField(max_length=100,unique=True, primary_key= True, blank=False)
+    rel_name = models.CharField(max_length=155, unique=False, blank=False)
+
+class ImageLabels(models.Model):
+    image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
+    label_id = models.ForeignKey(Label, on_delete=models.CASCADE)
+
+class ImageRelationship(models.Model):
+    label_id_1 = models.ForeignKey(Label, on_delete=models.CASCADE, related_name='label_id_1')
+    label_id_2 = models.ForeignKey(Label, on_delete=models.CASCADE, related_name='label_id_2')
+    rel_id = models.ForeignKey(Relationship, on_delete=models.CASCADE)
