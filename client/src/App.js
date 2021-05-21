f5663cf7e0aeb65926c3f1004a265fd38fc98b3c
SamT <samueltang@MacBook-Pro.local>
2021-05-21 11:00:14 -0400

    image grid demo

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React, { useState } from "react"
 import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   const [input, setInput] = useState('')
   
   const onSubmit = (e) => {
@@ -17,6 +34,39 @@ const App = () => {
 
     setInput('')
   }
+
+  const tileData = [
+    {
+      img: 'images/1.jpg',
+      title: 'Image',
+      author: 'author',
+      cols: 2,
+    },
+    {
+      img: 'images/2.jpg',
+      title: 'Image',
+      author: 'author',
+      cols: 1,
+    },
+    {
+      img: 'images/3.jpg',
+      title: 'Image',
+      author: 'author',
+      cols: 3,
+    },
+    {
+      img: 'images/4.jpg',
+      title: 'Image',
+      author: 'author',
+      cols: 1,
+    },
+    {
+      img: 'images/5.jpg',
+      title: 'Image',
+      author: 'author',
+      cols: 2,
+    },
+  ]
   
   return(
     <div>
@@ -27,6 +77,16 @@ const App = () => {
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <br />
+      <div className={classes.root}>
+        <GridList cellHeight={160} className={classes.gridList} cols={3}>
+          {tileData.map((tile) => (
+            <GridListTile key={tile.img} cols={tile.cols || 1}>
+              <img src={tile.img} alt={tile.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
     </div>
   )
 }
