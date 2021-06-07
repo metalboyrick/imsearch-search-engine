319270ab9bfed3a71fe2bcedd5fac3c9abc9bf69
Rick <rsulisthio@gmail.com>
2021-06-08 01:06:20 +0700

    Added firebase cloud

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from "react"
+import firebase from "firebase/app";
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import "firebase/storage";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { InputAdornment } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmHzDB9jnvzTrOCSLjotOb9fjxc3PqcnQ",
+  authDomain: "imsearch-f6bac.firebaseapp.com",
+  projectId: "imsearch-f6bac",
+  storageBucket: "imsearch-f6bac.appspot.com",
+  messagingSenderId: "431383682614",
+  appId: "1:431383682614:web:dc98b0bb0f1a46ebf94297"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// reference to cloud storage
+let storage = firebase.storage()
+
+// Create a storage reference from our storage service
+var storageRef = storage.ref();
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,29 +55,49 @@ const App = () => {
   const classes = useStyles();
   const [input, setInput] = useState('')
   const [imgData, setImgData] = useState([])
-  
+  const [waiting, setWaiting] = useState(false)
+  const [message, setMessage] = useState('')
+
+ 
   const onSubmit = (e) => {
     e.preventDefault()
     console.log("searching for: ", input)
 
-    setImgData([])
+    
+    setWaiting(true)
     axios({
       method: "GET",
       url: `http://127.0.0.1:8000/search?query=${input}`
     })
-      .then(res => {
+      .then(async (res) => {
         
-        // console.log(res);
-        // console.log(res.data);
-        setImgData(res.data.slice(0, 20))
+        setImgData([])
+        setMessage('')
+
+        let downloadURLs = []
+        for (var i = 0; i < 20; i++) {
+          try{
+            var url = await storageRef.child(`validation/${res.data[i]}`).getDownloadURL()
+          } catch (e) {
+            continue
+          }
+          downloadURLs.push(url)
+          setImgData(downloadURLs)
+        } 
+
+        if (downloadURLs.length == 0){
+          setMessage('Picture not found!')
+        }
+        
         setInput('')
+        setWaiting(false)
       })
 
     
   }
 
   const tileData = imgData.map(img => ({ 
-        img: `images/${img}`,
+        img: `${img}`,
         title: 'Image',
         author: 'author',
         cols: 1,
@@ -66,10 +112,27 @@ const App = () => {
         variant="outlined"
         InputProps={{type: 'search' }}
         onChange={(e) => setInput(e.target.value)}
-        InputProps={{endAdornment: <Button variant="contained" color="secondary" onClick={onSubmit}>
-        enter
-      </Button>}}
+        InputProps={{endAdornment:  <InputAdornment position='end'>
+         <Button variant="contained" color="secondary" onClick={onSubmit}>
+          enter
+        </Button>
+        <Box p={1} bgcolor="background.paper"></Box> 
+        {
+          waiting == true ? 
+          <CircularProgress color="secondary" /> :
+          <div />
+        }
+      </InputAdornment>
+       }}
       />
+
+      
+
+      {
+        message != '' ?
+        <p>{message}</p> :
+        <div/>
+      }
 
       <br />
       <div className={classes.root}>
