18111e1dd9dc0386d48a95b57588346ac25c4714
Rick <rsulisthio@gmail.com>
2021-06-07 23:51:46 +0700

    Update gitignore

--- server/imsearch/search_service/apps.py
+++ server/imsearch/search_service/apps.py
@@ -6,5 +6,5 @@ class SearchServiceConfig(AppConfig):
     
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search_service'
-    # sbert_model = SentenceTransformer('average_word_embeddings_glove.6B.300d')
+    sbert_model = SentenceTransformer('average_word_embeddings_glove.6B.300d')
 
